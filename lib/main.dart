364a5b9a488ca387a2d3a3cae2645d2a1eab1bfa
Raka Fajar Salinggih <raka@eudeka.id>
2020-12-04 22:32:33 +0700

    List Resep Makanan

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,140 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              'https://images.all-free-download.com/images/graphiclarge/fast_food_banners_colorful_vertical_roll_up_design_6843671.jpg',
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Trending Recipes",
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            listProductCard(),
+            listProductCard(),
+            listProductCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget listProductCard() {
+    return Stack(
+      children: [
+        Container(
+          height: 150,
+          margin: EdgeInsets.fromLTRB(
+            20.0,
+            5.0,
+            20.0,
+            5.0,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              12.0,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+              160.0,
+              10.0,
+              10.0,
+              20.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 120,
+                      child: Text(
+                        "Coklat tiga rasa enak mantap lezat",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.pinkAccent,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: List<Widget>.generate(
+                    5,
+                    (index) => Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "45 min",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    Icon(
+                      Icons.timer,
+                      color: Colors.black54,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 5.0,
+          left: 18.0,
+          bottom: 5.0,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(
+              12.0,
+            ),
+            child: Image.network(
+              'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1542062283%2Fchocolate-and-cream-layer-cake-1812-cover.jpg%3Fitok%3DrEWL7AIN',
+              width: 150,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
